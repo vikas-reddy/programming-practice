b1a0026ff5ebf84846e91b38ae25f190ae5c289a
Vikas Reddy <vikasreddy.com@gmail.com>
2014-11-08 18:34:26 +0530

    added bt template

---
+++ bt-00-template.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+struct node *newNode(int data) {
+  struct node *n = (struct node*)malloc(sizeof(struct node));
+  n->data = data;
+  n->left = NULL;
+  n->right = NULL;
+  return n;
+}
+
+void inOrder(struct node *n) {
+  if (n == NULL) {
+    return;
+  }
+  inOrder(n->left);
+  printf("%d ", n->data);
+  inOrder(n->right);
+}
+
+struct node *createTree() {
+  struct node *root = newNode(1);
+  root->left = newNode(2);
+  root->left->left = newNode(4);
+  root->left->right = newNode(5);
+  root->left->right->left = newNode(8);
+  root->left->right->right = newNode(9);
+  root->right = newNode(3);
+  root->right->left = newNode(6);
+  root->right->right = newNode(7);
+  root->right->right->right = newNode(10);
+  return root;
+}
+
+int main(int argc, const char *argv[])
+{
+  struct node *root = createTree();
+
+  inOrder(root);
+  printf("\n");
+
+  return 0;
+}
