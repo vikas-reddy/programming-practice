967b22c184a5a22b5ce210d23a05b5ccdc48ab1f
Vikas Reddy <vikasreddy.com@gmail.com>
2014-11-17 20:39:05 +0530

    DP: LCS with bottom-up

--- dp-01-lcs.cpp
+++ dp-01-lcs.cpp
@@ -15,12 +15,13 @@ using namespace std;
  */
 
 char *string1;
-char **arr;
+char **lcs_arr;
+char **path_arr;
 
 void print2Darray(int len1, int len2) {
   for (int i = 0; i < len1; i++) {
     for (int j = 0; j < len2; j++) {
-      printf("%c ", arr[i][j]);
+      printf("%c ", path_arr[i][j]);
     }
     printf("\n");
   }
@@ -29,7 +30,7 @@ void printSubstring(int len1, int len2) {
   int y = len1 - 1, x = len2 - 1;
 
   while (y >= 0) {
-    switch (arr[y][x]) {
+    switch (path_arr[y][x]) {
       case '\\':
         printf("%c", string1[y]);
         y--; x--;
@@ -46,20 +47,46 @@ int max (int a, int b) {
   return (a > b ? a : b);
 }
 
-int lcs(char str1[], char str2[], int len1, int len2) {
+// DP: builds the lcs_arr[len1 + 1][len2 + 1] array from bottom to top
+int lcs2(char str1[], char str2[], int len1, int len2) {
+  for (int i = 0; i < len1; i++) {
+    for (int j = 0; j < len2; j++) {
+      if (i == 0 || j == 0) {
+        lcs_arr[i][j] = 0;
+      }
+      else if (str1[i - 1] == str2[j - 1]) {
+        lcs_arr[i][j] = lcs_arr[i - 1][j - 1] + 1;
+        path_arr[i - 1][j - 1] = '\\';
+      }
+      else {
+        lcs_arr[i][j] = max(lcs_arr[i - 1][j], lcs_arr[i][j - 1]);
+        if (lcs_arr[i][j] == lcs_arr[i - 1][j]) {
+          path_arr[i - 1][j - 1] = '|';
+        }
+        else {
+          path_arr[i - 1][j - 1] = '-';
+        }
+      }
+    }
+  }
+  return lcs_arr[len1-1][len2-1];
+}
+
+// DP: normal recursive approach; takes exponential time
+int lcs1(char str1[], char str2[], int len1, int len2) {
   if (len1 == 0 || len2 == 0) {
     return 0;
   }
 
   if (str1[len1 - 1] == str2[len2 - 1]) {
-    arr[len1 - 1][len2 - 1] = '\\';
-    return 1 + lcs(str1, str2, len1 - 1, len2 - 1);
+    path_arr[len1 - 1][len2 - 1] = '\\';
+    return 1 + lcs1(str1, str2, len1 - 1, len2 - 1);
   }
   else {
-    int a = lcs(str1, str2, len1 - 1, len2);
-    int b = lcs(str1, str2, len1, len2 - 1);
+    int a = lcs1(str1, str2, len1 - 1, len2);
+    int b = lcs1(str1, str2, len1, len2 - 1);
     int maxLcs = max(a, b);
-    arr[len1 - 1][len2 - 1] = (maxLcs == a) ? '|' : '-';
+    path_arr[len1 - 1][len2 - 1] = (maxLcs == a) ? '|' : '-';
     return maxLcs;
   }
 }
@@ -78,13 +105,25 @@ int main(int argc, char *argv[])
   int len1 = strlen(str1);
   int len2 = strlen(str2);
 
-  // initializing the global array
-  arr = new char*[len1];
+  // initializing the path_arr[len1][len2] 2d array
+  path_arr = new char*[len1];
   for (int i = 0; i < len1; i++) {
-    arr[i] = new char[len2];
+    path_arr[i] = new char[len2];
   }
 
-  cout << lcs(str1, str2, len1, len2) << endl;
+  /*
+  cout << lcs1(str1, str2, len1, len2) << endl;
+  print2Darray(len1, len2);
+  printSubstring(len1, len2);
+  */
+
+  // initializing the lcs_arr[len1 + 1][len2 + 1] 2d array
+  lcs_arr = new char*[len1 + 1];
+  for (int i = 0; i < len1 + 1; i++) {
+    lcs_arr[i] = new char[len2 + 1];
+  }
+
+  cout << lcs2(str1, str2, len1 + 1, len2 + 1) << endl;
   print2Darray(len1, len2);
   printSubstring(len1, len2);
   return 0;
