4a341191a41667fde2d294829c4968fe8de8c432
Vikas Reddy <vikasreddy.com@gmail.com>
2014-12-07 16:27:18 +0530

    upgraded replace_zeroes_with_fives

--- replace_zeros_with_fives.cpp
+++ replace_zeros_with_fives.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
+#include<iostream>
+#include<stdlib.h>
 
 using namespace std;
 
+void changeNumber (int *n, int k) {
+  // Start from the tens digit (LSD) and go upto MSD
+  for (int den = 1; *n/den; den *= 10) {
+    if ( (*n/den) % 10 == 0 ) {
+      *n += k * den;
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cout << "Needs a command-line arg" << endl;
+    return 1;
+  }
+
+  int n = atoi(argv[1]);
+  int k = 5;
+
+  changeNumber(&n, k);
+  cout << n << endl;
+  /*
+  // Old code
   int num, n, k;
   cin >> num;
 
@@ -14,5 +36,6 @@ int main(int argc, char *argv[]) {
   }
 
   cout << n << endl;
+  */
   return 0;
 }
