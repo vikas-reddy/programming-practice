6f4a4db34a25cf662535fad5bb598885613d242d
Vikas Reddy <vikasreddy.com@gmail.com>
2015-01-01 17:49:46 +0530

    Array: rearranging negative and positive numbers in alternate fashion

--- array/array-23-rearrange-in-alt-fashion.cpp
+++ array/array-23-rearrange-in-alt-fashion.cpp
@@ -26,7 +26,7 @@ void printArray (int arr[], int n) {
   printf("\n");
 }
 
-void rearrange (int arr[], int n) {
+void rearrange1 (int arr[], int n) {
   int a = 0, b = 0;
 
   // Sort the array in such a way that positive numbers appear at the end, and
@@ -59,6 +59,97 @@ void rearrange (int arr[], int n) {
   }
 }
 
+// Left rotates arr[] by 1
+void rightRotate (int arr[], int f, int l) {
+  if (f >= l)
+    return;
+  int temp = arr[l];
+  for (int i = l; i > f; i--)
+    arr[i] = arr[i-1];
+  arr[f] = temp;
+}
+
+// O(n)
+void rearrange2 (int arr[], int n) {
+  int a, b;
+  a = 0;
+
+  // putting all negative numbers at the beginning
+  // O(n) as a and b traverse through the length of the array only once
+  while (true) {
+    // Find the first non-negative number
+    for (     ; a < n && arr[a] < 0;  a++);
+
+    // Find the first negative number
+    for (b = a; b < n && arr[b] >= 0; b++);
+
+    if (a >= n || b >= n || a >= b)
+      break;
+
+    // Rotate the subarray by 1 to get the violating -ve number to the
+    // beginning
+    rightRotate(arr, a, b);
+
+    // Start from the next element
+    a++;
+  }
+
+  int neg_count = a, pos_count = n - a;
+
+  if (neg_count == 0 || pos_count == 0)
+    return;
+
+  a = 1;
+  b = neg_count;
+
+  // Rearranging in alternate fashion
+  // O(n)
+  while (a < n && b < n && a < b) {
+    rightRotate(arr, a, b);
+    a += 2;
+    b += 1;
+  }
+}
+
+int findNextViolation (int arr[], int n, int a) {
+  while ( a < n &&
+          ((a & 1) && (arr[a] >= 0) || !(a & 1) && (arr[a] < 0))
+      ) a++;
+  return a;
+}
+
+int findNextPositive (int arr[], int n, int i) {
+  while (i < n && arr[i] < 0)
+    i++;
+  return i;
+}
+
+int findNextNegative (int arr[], int n, int i) {
+  while (i < n && arr[i] >= 0)
+    i++;
+  return i;
+}
+
+// Only one rearranging
+void rearrange3 (int arr[], int n) {
+  int a, b;
+  a = 0;
+
+  while (true) {
+    // Find first violation
+    a = findNextViolation(arr, n, a);
+
+    // Find b
+    b = (arr[a] < 0) ? findNextPositive(arr, n, a) : findNextNegative(arr, n, a);
+
+    if (a >= n || b >= n || a >= b)
+      break;
+
+    rightRotate(arr, a, b);
+    a++;
+  }
+}
+
 int main(int argc, const char *argv[])
 {
   // Random seed
@@ -68,12 +159,21 @@ int main(int argc, const char *argv[])
   /* int n = sizeof(arr)/sizeof(arr[0]); */
   int n = 10;
   int *arr = new int[n];
-  for (int i = 0; i < n; i++)
-    arr[i] = rand() % 100 - 50;
+  int *arr2 = new int[n];
+  for (int i = 0; i < n; i++) {
+    int num = rand() % 100 - 50;
+    arr[i] = num;
+    arr2[i] = num;
+  }
 
   printArray (arr, n);
-  rearrange (arr, n);
+  printArray (arr2, n);
+
+  rearrange1 (arr, n);
+  rearrange3 (arr2, n);
+
   printArray (arr, n);
+  printArray (arr2, n);
 
   return 0;
 }
